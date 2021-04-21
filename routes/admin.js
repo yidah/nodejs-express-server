0b460516fb5a4fe0e8ee6994520d1978304540f5
Yidah <yidahit@hotmail.com>
2021-04-21 14:59:20 +0100

    Use of ejs template

--- routes/admin.js
+++ routes/admin.js
@@ -10,20 +10,25 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    // res.sendFile(path.join(rootDir,'views','add-product.html'));
-    res.render('add-product', {docTitle: 'Add product', path:'admin/add-product', formsCSS:true, productCSS:true, activeAddProduct:true});
+  // res.sendFile(path.join(rootDir,'views','add-product.html'));
+  res.render('add-product', {
+    docTitle: 'Add product',
+    path: 'admin/add-product',
+    // formsCSS: true,
+    // productCSS: true,
+    // activeAddProduct: true,
+  });
 });
 
 // /admin/add-product => POST
 // filter for only post requests
 router.post('/add-product', (req, res, next) => {
   console.log(req.body);
-  products.push({title: req.body.title})
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
 // export router
 // module.exports = router;
 exports.routes = router;
-exports.products =products;
-
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -13,15 +13,15 @@ router.get('/', (req, res, next) => {
   const products = adminData.products;
   // render - is a express method.Uses the default templated engine
   // then we pass data we need to share with the view
-  // res.render('shop', {prods:products, docTitle: 'Shop'});
-  res.render('shop', {
-    prods: products,
-    docTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS:true,
-  });
+  res.render('shop', {prods:products, docTitle: 'Shop',  path: '/'});
+  // res.render('shop', {
+  //   prods: products,
+  //   docTitle: 'Shop',
+  //   path: '/',
+  //   hasProducts: products.length > 0,
+  //   activeShop: true,
+  //   productCSS:true,
+  // });
 });
 
 module.exports = router;
