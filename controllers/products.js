fc2aee48c15b26034eb54f31be79122eec9717d7
Yidah <yidahit@hotmail.com>
2021-04-26 16:46:07 +0100

    Fetching data from JSON files

--- controllers/products.js
+++ controllers/products.js
@@ -1,27 +1,27 @@
-const Product =require('../models/product');
+const Product = require('../models/product');
 
-exports.getAddProduct =(req, res, next) => {
-    // res.sendFile(path.join(rootDir,'views','add-product.html'));
-    res.render('add-product', {
-      docTitle: 'Add product',
-      path: 'admin/add-product',
-      // formsCSS: true,
-      // productCSS: true,
-      // activeAddProduct: true,
-    });
-  };
+exports.getAddProduct = (req, res, next) => {
+  // res.sendFile(path.join(rootDir,'views','add-product.html'));
+  res.render('add-product', {
+    docTitle: 'Add product',
+    path: 'admin/add-product',
+    // formsCSS: true,
+    // productCSS: true,
+    // activeAddProduct: true,
+  });
+};
 
-  exports.postAddNewProduct =(req, res, next) => {
-    const product = new Product(req.body.title);
-    product.save();
-    res.redirect('/');
-  };
+exports.postAddNewProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect('/');
+};
 
-  exports.getProducts =  (req, res, next) => {
-    const products = Product.fetchAll();
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
     // render - is a express method.Uses the default templated engine
     // then we pass data we need to share with the view
-    res.render('shop', {prods:products, docTitle: 'Shop',  path: '/'});
+    res.render('shop', { prods: products, docTitle: 'Shop', path: '/' });
     // res.render('shop', {
     //   prods: products,
     //   docTitle: 'Shop',
@@ -30,4 +30,5 @@ exports.getAddProduct =(req, res, next) => {
     //   activeShop: true,
     //   productCSS:true,
     // });
-  };
+  });
+};
--- models/product.js
+++ models/product.js
@@ -1,17 +1,44 @@
-const products =[];
+const fs = require('fs');
+const path = require('path');
+// const products = [];
+// path.dirname returns directory name of a path
+// require.main.filename gives us the path to the start up file, app.js
+const p = path.join(
+  path.dirname(require.main.filename),
+  'data',
+  'products.json'
+);
+
 module.exports = class Product {
-    constructor(t){
-        this.title = t;
-    }
+  constructor(t) {
+    this.title = t;
+  }
 
-    save(){
-        products.push(this);
-    }
+  save() {
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      // We avoid to lose the context of "this"
+      // by using an arrow function as a callback (err,fileContent)=>
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
 
-    //It is not call on a single instance of a product because it should fetch all products
-    //so static makes sure that we can call this method directly on the class itself and not on an instance
-    static fetchAll(){
-        return products;
-    }
-
-}
+  //It is not call on a single instance of a product because it should fetch all products
+  //so static makes sure that we can call this method directly on the class itself and not on an instance
+  // As fs.readFile is an async call we use a callbackFunction so that whenever we finished reading the
+  // file we can send the returned data to our function (see products.js controller)
+  static fetchAll(callbackFunction) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        callbackFunction([]);
+      }
+      callbackFunction(JSON.parse(fileContent));
+    });
+  }
+};
